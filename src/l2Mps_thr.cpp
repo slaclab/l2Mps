2db1e574db8388667ce8c5688099d9a32ac7961d
Jesus Vasquez <jvasquez@slac.stanford.edu>
2019-07-31 13:12:57 -0700

    Fix formatting

--- src/l2Mps_thr.cpp
+++ src/l2Mps_thr.cpp
@@ -111,13 +111,13 @@ void IThrChannel::readThrChData(thr_chData_t& data) const
         tableData.maxEn = (it->second).maxEn.get();
 
         std::pair<bool,uint32_t> tmp ( (it->second).min.get() );
-        tableData.min   = std::make_pair( tmp.first, ( static_cast<int32_t>(tmp.second) - scaleOffset ) * scaleSlope );
-	tableData.minRaw = std::make_pair( tmp.first, (static_cast<int32_t>(tmp.second)));
+        tableData.min    = std::make_pair( tmp.first, ( static_cast<int32_t>(tmp.second) - scaleOffset ) * scaleSlope );
+        tableData.minRaw = std::make_pair( tmp.first, ( static_cast<int32_t>(tmp.second) ) );
 
-	tmp = (it->second).max.get();
-        tableData.max   = std::make_pair( tmp.first, ( static_cast<int32_t>(tmp.second) - scaleOffset ) * scaleSlope );
-	tableData.maxRaw = std::make_pair( tmp.first, (static_cast<int32_t>(tmp.second)));
-	
+        tmp = (it->second).max.get();
+        tableData.max    = std::make_pair( tmp.first, ( static_cast<int32_t>(tmp.second) - scaleOffset ) * scaleSlope );
+        tableData.maxRaw = std::make_pair( tmp.first, ( static_cast<int32_t>(tmp.second) ) );
+
         data.insert( std::make_pair(it->first, tableData) );
     }
 }
