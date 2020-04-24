f38f2557f8785af2fb090d606ae18dd1a8d93a9a
Jesus Vasquez <jesusvasquez333@gmail.com>
2020-04-24 12:18:17 -0700

    Add skeleton of new class to interface to the LN soft inputs

---
+++ src/l2Mps_soft_inputs.cpp
@@ -0,0 +1,66 @@
+/**
+ *-----------------------------------------------------------------------------
+ * Title      : Link Node Software Inputs class
+ * ----------------------------------------------------------------------------
+ * File       : l2Mps_blm.h
+ * Author     : Jesus Vasquez, jvasquez@slac.stanford.edu
+ * Created    : 2020-04-24
+ * ----------------------------------------------------------------------------
+ * Description:
+ * Class for interfacing with the Link Node Software Inputs
+ * ----------------------------------------------------------------------------
+ * This file is part of l2Mps. It is subject to
+ * the license terms in the LICENSE.txt file found in the top-level directory
+ * of this distribution and at:
+    * https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+ * No part of l2Mps, including this file, may be
+ * copied, modified, propagated, or distributed except according to the terms
+ * contained in the LICENSE.txt file.
+ * ----------------------------------------------------------------------------
+**/
+
+#include "l2Mps_soft_inputs.h"
+
+MpsDigitalMessage IMpsSoftInputs::create(Path root)
+{
+    if(!root)
+        throw std::runtime_error("The root Path is empty");
+
+    return boost::make_shared<IMpsSoftInputs>(root);
+}
+
+IMpsSoftInputs::IMpsSoftInputs(Path root)
+:
+    swBitValue ( root, MpsDigitalMessageModuleName + "SwBitValue" ),
+    swBitError ( root, MpsDigitalMessageModuleName + "SwBitError" ),
+    numInputs  ( swBitValue.getSizeBits() )
+{
+    // Verify that both register has the same number of bits
+    if ( numInputs != swBitError.getSizeBits() )
+        throw std::runtime_error("SwBitValue and SwBitError has different sizes")
+}
+
+std::pair<bool, bool> IMpsSoftInputs::getInput(std::size_t index) const
+{
+    if ( index > numInputs )
+        throw std::runtime_error("Trying to access a software input out of bounds")
+
+}
+
+bool IMpsSoftInputs::setInput(bool val, std::size_t index) const
+{
+    if ( index > numInputs )
+        throw std::runtime_error("Trying to access a software input out of bounds")
+}
+
+std::pair<bool, bool> IMpsSoftInputs::getErrorInput(std::size_t index) const
+{
+    if ( index > numInputs )
+        throw std::runtime_error("Trying to access a software input out of bounds")
+}
+
+bool IMpsSoftInputs::setErrorInput(bool val, std::size_t index) const
+{
+    if ( index > numInputs )
+        throw std::runtime_error("Trying to access a software input out of bounds")
+}
---
+++ src/l2Mps_soft_inputs.h
@@ -0,0 +1,56 @@
+#ifndef L2MPS_SOFT_INPUTS_H
+#define L2MPS_SOFT_INPUTS_H
+
+/**
+ *-----------------------------------------------------------------------------
+ * Title      : Link Node Software Inputs class
+ * ----------------------------------------------------------------------------
+ * File       : l2Mps_blm.h
+ * Author     : Jesus Vasquez, jvasquez@slac.stanford.edu
+ * Created    : 2020-04-24
+ * ----------------------------------------------------------------------------
+ * Description:
+ * Class for interfacing with the Link Node Software Inputs
+ * ----------------------------------------------------------------------------
+ * This file is part of l2Mps. It is subject to
+ * the license terms in the LICENSE.txt file found in the top-level directory
+ * of this distribution and at:
+    * https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+ * No part of l2Mps, including this file, may be
+ * copied, modified, propagated, or distributed except according to the terms
+ * contained in the LICENSE.txt file.
+ * ----------------------------------------------------------------------------
+**/
+
+#include "l2Mps_cpsw.h"
+
+class IMpsSoftInputs;
+
+typdef boost::shared_ptr<IMpsSoftInputs> MpsSoftInputs;
+
+// Name of the Digital Message module
+const std::string MpsDigitalMessageModuleName("AppTop/AppCore/MpsLinkNodeCore/MpsDigitalMessage/");
+
+class IMpsSoftInputs
+{
+public:
+    IMpsSoftInputs(Path root);
+
+    // Factory method, which returns a smart pointer
+    static MpsDigitalMessage create(Path root);
+
+    // Input values
+    std::pair<bool, bool> getInput(std::size_t index)               const;
+    bool                  setInput(bool val, std::size_t index)     const;
+
+    // Input error values
+    std::pair<bool, bool> getErrorInput(std::size_t index)           const;
+    bool                  setErrorInput(bool val, std::size_t index) const;
+
+private:
+    CpswRegRW<uint16_t> swBitValue; // Register to set values
+    CpswRegRW<uint16_t> swBitError; // Register with values used during timeouts
+    std::size_t         numInputs;  // Number of software bits
+};
+
+#endif
